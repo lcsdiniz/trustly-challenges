4b1361a282eb37ff6d9112b9f2a9affb3c796838
Lucas Diniz <lucas.henrique@globalsys.com>
2021-09-03 01:43:08 -0300

    Remoção de comentário

--- trustly-challenge-2/src/main.java
+++ trustly-challenge-2/src/main.java
@@ -3,8 +3,6 @@ public class main {
 
 	public static void main(String[] args) {
 		InputScreen inputScreen  = new InputScreen();
-		
-		//Person person = new Person("Lucas Diniz", "Brazilian", "Become a reference in his field");
 	}
 
 }
